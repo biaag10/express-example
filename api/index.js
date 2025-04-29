09afdcb6abc457fbda710fb58adc29b32e4e2ab0
Bianca Andrade Galvão <107511894+biaag10@users.noreply.github.com>
2025-04-29 21:06:51 +0000

    teste

--- api/index.js
+++ api/index.js
@@ -1,20 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import db from './database/configdb.js';
-import User from './models/User.js'; // cria a collection de users
+import User from './models/User.js';
 
 dotenv.config();
 db.connect();
 
 const app = express();
-app.use(express.json()); // para aceitar JSON no corpo das requisições
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send({message: 'API is running...'});
+  res.send({ message: 'API is running locally...' });
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta http://localhost:${PORT}/`);
-});
+if (process.env.VERCEL) {
+  module.exports = app; // Exporte a app para o Vercel
+} else {
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta http://localhost:${PORT}`);
+  });
+}
