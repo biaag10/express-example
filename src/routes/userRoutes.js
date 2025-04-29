df4634e919b3b028d7e2204dfaa4d22f386dee76
Bianca Andrade Galvão <107511894+biaag10@users.noreply.github.com>
2025-04-29 15:04:49 +0000

    comentarios nos arquivos que nao estao sendo utilizados

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,16 +1,16 @@
-import express from 'express';
-import { register, login, protectedRoute } from '../controllers/userController.js';
-import { authenticateToken } from '../middlewares/authMiddleware.js';
+// import express from 'express';
+// import { register, login, protectedRoute } from '../controllers/userController.js';
+// import { authenticateToken } from '../middlewares/authMiddleware.js';
 
-const router = express.Router();
+// const router = express.Router();
 
-// Rota para registro de usuário
-router.post('/register', register);
+// // Rota para registro de usuário
+// router.post('/register', register);
 
-// Rota para login de usuário
-router.post('/login', login);
+// // Rota para login de usuário
+// router.post('/login', login);
 
-// Rota protegida, que só pode ser acessada com token válido
-router.get('/protected', authenticateToken, protectedRoute);
+// // Rota protegida, que só pode ser acessada com token válido
+// router.get('/protected', authenticateToken, protectedRoute);
 
-export default router;
+// export default router;
