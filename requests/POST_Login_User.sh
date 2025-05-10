a20c7203845716e9840bf7d49fc7fa0ba2e0f8ec
biaag10 <107511894+biaag10@users.noreply.github.com>
2025-05-10 13:10:27 -0300

    ajuste vercel.json

--- requests/POST_Login_User.sh
+++ requests/POST_Login_User.sh
@@ -2,7 +2,7 @@
 
 # login bem-sucedido (com username)
 curl --request POST \
-  --url 'http://localhost:3000/users/login' \
+  --url 'https://express-backend-mongodb-cug4k8kck-bianca-ags-projects.vercel.app/users/login' \
   --header 'Content-Type: application/json' \
   --data '{
     "username": "Bianca123",
