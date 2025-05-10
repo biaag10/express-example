55a3b6066a90eb95128691bc3a9e7187da357043
Bianca Andrade Galvão <107511894+biaag10@users.noreply.github.com>
2025-05-10 18:29:15 +0000

    comentarios ajustados

--- api/routes/workout.route.js
+++ api/routes/workout.route.js
@@ -4,13 +4,13 @@ import workoutController from "../controllers/workout.controller.js";
 
 const router = express.Router();
 
-router.use(verifyToken); // Protege todas as rotas abaixo com o middleware de verificação de token
+router.use(verifyToken); // protege todas as rotas abaixo com o middleware de verificação de token
 
-router.post('/register', verifyToken, workoutController.createWorkout); // Criar um novo treino
-router.get('/all-workouts', verifyToken, workoutController.getAllWorkouts); // Obter todos os treinos do usuário
-router.get('/get-workout-id/:id', verifyToken, workoutController.getWorkoutById); // Obter um treino específico pelo ID
-router.put('/update-all-workout/:id', verifyToken, workoutController.updateWorkout); // Atualizar um treino específico pelo ID
-router.patch('/update-workout/:id', verifyToken, workoutController.updateWorkout); // Atualizar parcialmente um treino específico pelo ID
-router.delete('/delete/:id', verifyToken, workoutController.deleteWorkout); // Deletar um treino específico pelo ID
+router.post('/register', verifyToken, workoutController.createWorkout); // cria um novo treino
+router.get('/all-workouts', verifyToken, workoutController.getAllWorkouts); // busca todos os treinos do usuário
+router.get('/get-workout-id/:id', verifyToken, workoutController.getWorkoutById); // busca um treino específico pelo ID
+router.put('/update-all-workout/:id', verifyToken, workoutController.updateWorkout); // atualiza um treino específico pelo ID
+router.patch('/update-workout/:id', verifyToken, workoutController.updateWorkout); // atualiza parcialmente um treino específico pelo ID
+router.delete('/delete/:id', verifyToken, workoutController.deleteWorkout); // deleta um treino específico pelo ID
 
 export default router;
--- api/services/user.service.js
+++ api/services/user.service.js
@@ -2,32 +2,32 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
-// Função para registrar um novo usuário
+// função para registrar um novo usuário
 const register = async (name, username, email, password) => {
-    // validação de formato de email
+    // valida o formato de email
     const emailRegex = /^\S+@\S+\.\S+$/;
     if (!email.match(emailRegex)) {
         throw new Error('Formato de e-mail inválido');
     }
 
-    // validação de formato de senha (mínimo 8 caracteres, 1 maiúscula, 1 número, 1 especial)
+    // valida o formato de senha (mínimo 8 caracteres, 1 maiúscula, 1 número, 1 especial)
     const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     if (!password.match(passwordRegex)) {
         throw new Error('A senha deve ter no mínimo 8 caracteres, uma letra maiúscula, um número e um caractere especial');
     }
 
-    // Verifica se o e-mail ou o nome de usuário já existem
+    // verifica se o e-mail ou o nome de usuário já existem
     const userExists = await User.findOne({ $or: [{ email }, { username }] });
     if (userExists) {
         throw new Error('E-mail ou nome de usuário já existe');
     }
 
-    // Criação da senha criptografada
+    // cria a senha criptografada
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
     try {
-        // Criação do usuário no banco de dados
+        // cria o usuário no banco de dados
         const newUser = await User.create({
             name,
             username,
@@ -40,7 +40,7 @@ const register = async (name, username, email, password) => {
     }
 };
 
-// Função para fazer login e gerar o token
+// função para fazer login e gerar o token
 const login = async (usernameOrEmail, password) => {
     const user = await User.findOne({ $or: [{ username: usernameOrEmail }, { email: usernameOrEmail }] }).select('+password');
 
@@ -53,7 +53,7 @@ const login = async (usernameOrEmail, password) => {
         throw new Error('Senha incorreta');
     }
 
-    // Criação do token JWT
+    // cria o token JWT
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
     return token;
 };
